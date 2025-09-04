42f95f3a4b272922b614e582768f8f9ea137338a
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-09-05 00:34:13 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (85.72%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        
+        // int count = 0;
+
+        // for(int i= 1 ; i<=n; i++){
+        //     for(int j=1; j<=m ;j++){
+        //         if((i+j)%2!=0){
+        //             count++;
+        //         }
+        //     }
+        // }
+    
+        // return count;
+
+
+        return ((long) n * (long) m) / 2;
+
+    }
+}
