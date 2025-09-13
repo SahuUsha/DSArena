d5dc5373c16863bdcd8e3bb132cf8e2fe3badae8
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-09-13 15:08:37 +0530

    Time: 1 ms (99.74%) | Memory: 47.5 MB (81.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -39,4 +39,36 @@ class Solution {
 
         return dp[i][j] = Math.min(up, left);
     }
+
+    public int tabulation(int grid[][]){
+        int n = grid.length;
+        int m = grid.length;
+        int dp[][] = new int[n][m];
+
+        for(int i =0 ; i<n ; i++){
+            for(int j =0 ; j<m ;j++){
+
+                if(i==0 && j==0) dp[i][j] = grid[i][j];
+
+                else{
+
+                    int up =0;
+                    int left =0;
+                    if(i>0) {
+                        up = grid[i][j] + dp[i-1][j];
+                    }
+
+                    if(j>0){
+                        left = grid[i][j] + dp[i-1][j];
+                    }
+
+
+                    dp[i][j] = Math.min(up,left);
+                }
+
+            }
+        } 
+      return dp[n-1][m-1];
+
+    }
 }
