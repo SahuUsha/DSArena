a11ac7b3ca50e283a8c26a5c672cc28ad23fab03
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-12-05 23:01:25 +0530

    Time: 1 ms (52.61%) | Memory: 43.1 MB (13.38%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int splitArray(int[] arr, int k) {
+         int low = Integer.MIN_VALUE;
+        int high =0;
+        for(int i =0; i<arr.length;i++){
+            low = Math.max(low,arr[i]);
+            high +=arr[i];
+        }
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            int cdiv = countDivide(arr,mid);
+            if(cdiv>k){
+                low = mid +1;
+                
+            }else{
+                high = mid -1;
+                
+            }
+        }
+        return low;
+        
+    }
+     public int  countDivide(int[] arr,int split){
+        int divide = 1;
+        
+        int paintSubbaray =0;
+        for(int i=0; i<arr.length;i++){
+            if(paintSubbaray+arr[i]<=split){
+                paintSubbaray += arr[i];
+            }else{
+                divide++;
+                paintSubbaray = arr[i];
+            }
+        }
+        return divide;
+    }
+}
