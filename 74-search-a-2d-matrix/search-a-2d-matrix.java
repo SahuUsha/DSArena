27b017cc1de6636caa78678fb946f592ac68bd5e
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-12-08 14:37:28 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (8.27%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,7 +1,31 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        for (int r = 0; r < matrix.length; r++) {
+        int low = 0;
+        int n =  matrix.length;
+        int m  = matrix[0].length;
+        int high = (n*m)-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            int row = mid/m;
+            int col = mid%m;
+            if(matrix[row][col]==target){
+                return true;
+
+            }
+            else if(matrix[row][col]>target){
+                 high = mid-1;
+            }else{
+                low = mid+1;
+            }
+
+        }
+     return false;
+      
+    }
+     public static boolean searxm(int[][] matrix, int target){
+           for (int r = 0; r < matrix.length; r++) {
             int[] row =matrix[r];
             int low = 0;
             int high = row.length - 1;
@@ -20,5 +44,6 @@ class Solution {
             }
         }
             return false;
+            
             }
 }
