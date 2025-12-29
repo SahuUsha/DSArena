5f49bdf52dd2ebbfd56818ccea2dd12944559c1e
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2025-12-29 11:56:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/One odd Occuring/one-odd-occuring.java
+++ Difficulty: Basic/One odd Occuring/one-odd-occuring.java
@@ -2,21 +2,34 @@ class Solution {
     // Method to find the element with odd occurrence in given array
     int getOddOccurrence(int[] arr) {
         // code here
-        HashSet<Integer> set = new HashSet<>();
+    //     HashSet<Integer> set = new HashSet<>();
         
-        for(int i = 0 ; i<arr.length ; i++){
-            if(set.contains(arr[i])){
-                set.remove(arr[i]);
+    //     for(int i = 0 ; i<arr.length ; i++){
+    //         if(set.contains(arr[i])){
+    //             set.remove(arr[i]);
                 
-            }else{
-                set.add(arr[i]);
-            }
-        }
+    //         }else{
+    //             set.add(arr[i]);
+    //         }
+    //     }
        
-       int ans = 0;
-       for(int e : set){
-           ans = e;
-       }
-        return ans;
+    //   int ans = 0;
+    //   for(int e : set){
+    //       ans = e;
+    //   }
+    //     return ans;
+    Arrays.sort(arr);
+    
+    int first = 0;
+    int sec = 1;
+    while((sec <=arr.length-2 )&& (arr[first]==arr[sec])){
+        first +=2;
+         sec +=2;
+    //   System.out.println("second"+sec);
+         
+    }
+    // System.out.println("fisrt"+first);
+    return arr[first];
+    
     }
 }
