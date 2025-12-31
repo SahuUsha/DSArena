608547b4fc18739b7077ec92aeddb110b772776f
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-01-01 01:13:23 +0530

    Time: 1 ms (87.81%) | Memory: 44.3 MB (34.83%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,21 +1,43 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
-        help(result , ans, nums, 0);
-        return result;
+        // List<List<Integer>> result = new ArrayList<>();
+        // List<Integer> ans = new ArrayList<>();
+        // help(result , ans, nums, 0);
+        // return result;
         
+        return bitmethod(nums);
     }
 
-    public static void help(List<List<Integer>> result , List<Integer> ans , int []nums , int start){
-        result.add(new ArrayList<>(ans));
+    // public static void help(List<List<Integer>> result , List<Integer> ans , int []nums , int start){
+    //     result.add(new ArrayList<>(ans));
 
-        for(int i = start ;i<nums.length ;i++){
+    //     for(int i = start ;i<nums.length ;i++){
             
-            ans.add(nums[i]);
-            help(result,ans, nums,i+1);
-            ans.remove(ans.size()-1);
+    //         ans.add(nums[i]);
+    //         help(result,ans, nums,i+1);
+    //         ans.remove(ans.size()-1);
 
+    //     }
+    // }
+
+     public static List<List<Integer>> bitmethod(int[] nums) {
+        int n = nums.length;
+        int sample = 1 << n;   // 2^n
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int num = 0; num < sample; num++) {
+            List<Integer> res = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                if ((num & (1 << i)) != 0) {   // ✅ correct bit check
+                    res.add(nums[i]);
+                }
+            }
+
+            ans.add(res);
         }
+        return ans;
     }
+
+
 }
