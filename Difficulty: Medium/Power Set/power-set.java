8e0290da76cb839c3e798db9c23efcb4749fb60e
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-02-28 22:23:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Power Set/power-set.java
+++ Difficulty: Medium/Power Set/power-set.java
@@ -3,30 +3,51 @@
 class Solution {
     public List<String> AllPossibleStrings(String s) {
         List<String> ans = new ArrayList<>();
-        StringBuffer rs = new StringBuffer();
         
-         PossibleStrings(s,0,ans,rs);
-         Collections.sort(ans);
+        PossibleStr(s,ans);
+        Collections.sort(ans);
+        
+
          return ans;
     }
     
-     public static void PossibleStrings(String s ,int idx, List<String> ans , StringBuffer rs){
-         if(idx==s.length()){
-               if (rs.length() > 0) {
-                ans.add(rs.toString());
-            }
-            return;
-         }
+    //  public static void PossibleStrings(String s ,int idx, List<String> ans , StringBuffer rs){
+    //      if(idx==s.length()){
+    //           if (rs.length() > 0) {
+    //             ans.add(rs.toString());
+    //         }
+    //         return;
+    //      }
          
          
          
-             rs.append(s.charAt(idx));
-             PossibleStrings(s,idx+1,ans, rs);
+    //          rs.append(s.charAt(idx));
+    //          PossibleStrings(s,idx+1,ans, rs);
              
-             rs.deleteCharAt(rs.length() - 1);
-              PossibleStrings(s,idx+1,ans, rs);
+    //          rs.deleteCharAt(rs.length() - 1);
+    //           PossibleStrings(s,idx+1,ans, rs);
              
          
-     }
+    //  }
   
+  
+    public void PossibleStr(String s ,List<String> ans){
+        
+        int n  = s.length();
+        
+        for(int i = 1 ; i<(1<<n);i++){
+            
+            StringBuilder str = new StringBuilder();
+            
+            for(int j = 0 ; j<n ;j++){
+                
+                if((i & (1 << j)) != 0){
+                    str.append(s.charAt(j));
+                }
+                
+            }
+            ans.add(str.toString());
+        }
+        
+    }
 }
