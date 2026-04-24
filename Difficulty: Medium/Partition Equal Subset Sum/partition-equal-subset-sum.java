0fdb24cf18e5914793d8b40f1514c30a2068d3a1
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-04-24 20:32:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -14,40 +14,45 @@ class Solution {
         
         int find = sum/2;
         
-        return subsetTab(arr,find);
+        return isPartition(arr,find);
         
         
         
     }
-    static boolean subsetTab(int arr[], int tar){
-        boolean dp[][] = new boolean[arr.length][tar+1];
+   static boolean isPartition(int arr[],int find){
+       
+       boolean  [][]dp = new boolean[arr.length][find+1];
+       
+       if(find>=arr[0]){
+           
+       dp[0][arr[0]] =true;
+       }
+       
+       for(int i = 0; i<arr.length;i++){
+           dp[i][0] = true;
+       }
+       
+       for(int i = 1; i<arr.length ;i++){
+           for(int tar = 1; tar<=find;tar++){
+               
+               boolean nottake = dp[i-1][tar];
+               
+               boolean take = false;
+               
+               if(tar>=arr[i]){
+                   take = dp[i-1][tar-arr[i]];
+               }
+               
+               dp[i][tar] = take|| nottake;
         
-        for(int i = 0 ;i<arr.length;i++){
-            dp[i][0] = true;
-        }
-        
-        if(tar>=arr[0]){
-            dp[0][arr[0]] =true;
-        }
-        
-        for(int i=1;i<arr.length;i++){
-            for(int target = 1 ; target<=tar ;target++){
-                boolean nottake = dp[i-1][target];
-                
-                boolean take = false;
-                
-                
-                if(target>=arr[i]){
-                    take = dp[i-1][target-arr[i]];
-                    
-                }
-                
-                dp[i][target] = nottake || take;
-                
-            }
-        }
-        
-        return dp[arr.length-1][tar];
-        
-    }
+               
+           }
+       }
+       
+       return dp[arr.length-1][find];
+       
+       
+   }
+    
+    
 }
