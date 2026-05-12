2a9976994026db3f020b37cf9eb3b50d1e776024
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-05-12 12:19:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -2,57 +2,29 @@ class Solution {
     static boolean equalPartition(int arr[]) {
         // code here
         
-        int sum = 0;
-        for(int i = 0;i<arr.length;i++){
-            sum += arr[i];
         
-        }
-        
-        if(sum%2!=0) {return false;}
-        
-        // System.out.println("Sumaiton :"+sum);
-        
-        int find = sum/2;
-        
-        return isPartition(arr,find);
-        
-        
-        
+      int totalSum = 0;
+      for(int i = 0 ; i<arr.length ;i++){
+          totalSum += arr[i];
+      }
+      
+      if(totalSum%2!=0){
+          return false;
+      }
+      
+      int target = totalSum/2;
+      
+      
+      
+      boolean dp[] = new boolean[target+1];
+      dp[0] = true;
+      
+      for(int num : arr){
+          for(int i = target; i>=num ;i--){
+              dp[i] = dp[i] || dp[i-num];
+          }
+      }
+      
+      return dp[target];
     }
-   static boolean isPartition(int arr[],int find){
-       
-       boolean  [][]dp = new boolean[arr.length][find+1];
-       
-       if(find>=arr[0]){
-           
-       dp[0][arr[0]] =true;
-       }
-       
-       for(int i = 0; i<arr.length;i++){
-           dp[i][0] = true;
-       }
-       
-       for(int i = 1; i<arr.length ;i++){
-           for(int tar = 1; tar<=find;tar++){
-               
-               boolean nottake = dp[i-1][tar];
-               
-               boolean take = false;
-               
-               if(tar>=arr[i]){
-                   take = dp[i-1][tar-arr[i]];
-               }
-               
-               dp[i][tar] = take|| nottake;
-        
-               
-           }
-       }
-       
-       return dp[arr.length-1][find];
-       
-       
-   }
-    
-    
 }
