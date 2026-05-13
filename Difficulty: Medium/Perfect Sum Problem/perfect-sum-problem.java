d644cf7b65c29265a0094d7517c24e2e9e2f2c9a
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-05-13 23:56:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -4,15 +4,17 @@ class Solution {
         // code here
         
         
-        int dp[][] = new int[nums.length][target+1];
+        // int dp[][] = new int[nums.length][target+1];
         
         
-        for(int[] pd : dp){
+        // for(int[] pd : dp){
             
-        Arrays.fill(pd,-1);
-        }
+        // Arrays.fill(pd,-1);
+        // }
         
-        return ksubset2(nums,target,0,dp);
+        // return ksubset2(nums,target,0,dp);
+        
+        return tabulation(nums,target);
         
         
     }
@@ -81,6 +83,42 @@ class Solution {
         dp[idx][target] = notake + take;
         
         return dp[idx][target];
+    }
+    
+    public int tabulation(int []nums,int target){
+        
+        int [][]dp = new int[nums.length][target+1];
+        
+        
+        if(nums[0]==0){
+            dp[0][0] = 2;
+        }else{
+            dp[0][0] =1;
+        }
+        
+        
+        if(nums[0] != 0 && nums[0]<=target){
+            dp[0][nums[0]] = 1;
+        }
+        
+        // for(int i = 0 ; i<nums.length;i++){
+        //     dp[i][0] = 1;
+        // }
+        
+        for(int i = 1 ;i<nums.length ;i++){
+            for(int tar = 0 ; tar<=target;tar++){
+                int notTake = dp[i-1][tar];
+                
+                int take =0;
+                
+                if(nums[i]<=tar){
+                    take = dp[i-1][tar-nums[i]];
+                }
+                
+                dp[i][tar] = notTake+take;
+            }
+        }
+        return dp[nums.length-1][target];
     }
     
 }
