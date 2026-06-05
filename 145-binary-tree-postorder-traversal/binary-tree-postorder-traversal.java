16e2c61d138ce559913226ef5b65176d42b271bd
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-06-05 21:41:12 +0530

    Time: 1 ms (12.82%) | Memory: 42.9 MB (91.86%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -33,7 +33,7 @@ class Solution {
     public List<Integer> InterativePostOrder(TreeNode root){
         ArrayList<Integer> list = new ArrayList<>();
         Stack<TreeNode> s1 = new Stack<>();
-        Stack<TreeNode> s2 = new Stack<>();
+        // Stack<TreeNode> s2 = new Stack<>();
         s1.push(root);
 
         while(!s1.isEmpty()){
@@ -43,7 +43,8 @@ class Solution {
                 continue;
             }
            
-            s2.push(node);
+            // s2.push(node);
+            list.add(node.val);
 
             if(node.left!=null){
                 s1.push(node.left);
@@ -52,11 +53,19 @@ class Solution {
                 s1.push(node.right);
             }
         }
-        int size = s2.size();
-        for(int i = 0; i<size;i++){
-            TreeNode r1 = s2.pop(); 
-            list.add(r1.val);
-       }
+    //     int size = s2.size();
+    //     for(int i = 0; i<size;i++){
+    //         TreeNode r1 = s2.pop(); 
+    //         list.add(r1.val);
+    //    }
+    int size = list.size()/2;
+    for(int i = 0; i<size;i++){
+        int temp = list.get(i);
+        list.set(i,list.get(list.size()-i-1));
+        list.set(list.size()-i-1, temp);
+    }
+
+    
         return list;
     }
 }
