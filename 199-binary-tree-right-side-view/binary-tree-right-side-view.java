cd99eb15d947b4a2c62fd117100a888bf9615d08
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-06-10 15:46:33 +0530

    Time: 2 ms (4.49%) | Memory: 43.6 MB (49.39%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -13,10 +13,21 @@
  *     }
  * }
  */
+
+ class Pair{
+    TreeNode node;
+    int level;
+    Pair(TreeNode node , int level){
+        this.node = node;
+        this.level = level;
+    }
+}
+
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
          List<Integer> List = new ArrayList<>();
-          RightSideView(root , 1 , List);
+        //   RightSideView(root , 1 , List);
+        rightView(root,List);
           return List;
     }
     int maxdepth = 0;
@@ -32,5 +43,39 @@ class Solution {
                RightSideView(root.right, cl+1 ,ll);
                RightSideView(root.left, cl+1 , ll);
         }
+
+
+// By BFS;
+
+         public void rightView(TreeNode root , List<Integer> list) {
+        // code here
+        HashMap<Integer,Integer> map = new HashMap<>();
+        Queue<Pair> q = new LinkedList<>();
+        
+        if(root==null) return;
+
+        q.offer(new Pair(root,0));
+        while(!q.isEmpty()){
+            Pair pq = q.poll();
+            
+            map.put(pq.level, pq.node.val);
+            
+            if(pq.node.left!=null){
+                q.offer(new Pair(pq.node.left,pq.level+1));
+            }
+            
+            if(pq.node.right!=null){
+                q.offer(new Pair(pq.node.right,pq.level+1));
+            }
+            
+        }
+        // ArrayList<Integer> list = new ArrayList<>();
+        
+        for(int m : map.values()){
+            list.add(m);
+        }
+        
+        // return list;
+    }
 
 }
