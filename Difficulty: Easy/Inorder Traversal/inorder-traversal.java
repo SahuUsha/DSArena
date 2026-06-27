31e7ce79f0055d067eaa22118ce63b7991fba2ea
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-06-27 15:35:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Inorder Traversal/inorder-traversal.java
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.java
@@ -11,9 +11,11 @@ class Node {
 class Solution {
     public ArrayList<Integer> inOrder(Node root) {
         // code here
-        ArrayList<Integer> list = new ArrayList<>();
-        return inOrder1(root, list);
+        // ArrayList<Integer> list = new ArrayList<>();
+        // return inOrder1(root, list);
         
+        
+        return morris(root);
         
     }
     
@@ -27,4 +29,34 @@ class Solution {
         
         return list;
     }
-}
+    
+    public ArrayList<Integer> morris(Node root){
+        ArrayList<Integer> list  = new ArrayList<>();
+        Node curr = root;
+        
+        while(curr!=null){
+            
+            if(curr.left==null){
+                 list.add(curr.data);
+                 curr = curr.right;
+            }else{
+
+               Node prev = curr.left;
+               while(prev.right!=null && prev.right!=curr){
+                   prev = prev.right;
+               }
+               
+               if(prev.right==null){
+                   prev.right = curr;
+                   curr = curr.left;
+               }else{
+                   prev.right=null;
+                   list.add(curr.data);
+                   curr = curr.right;  
+               }
+            }
+        }
+        
+        return list;
+    }
+} 
