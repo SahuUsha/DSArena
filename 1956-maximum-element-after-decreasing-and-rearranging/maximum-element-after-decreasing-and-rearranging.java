d30e2a2fecf81b7d678d217ae9e42856e1beddb5
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-06-29 00:55:30 +0530

    Time: 702 ms (6.67%) | Memory: 79.7 MB (5.00%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.java
@@ -4,17 +4,14 @@ class Solution {
         if(arr.length==1) return 1;
         
         Arrays.sort(arr);
-        
-        
-
         arr[0] =1;
        
+
         for(int i = 1 ; i<arr.length ; i++){
-        
             System.out.println("ind : "+i+" : "+arr[i]);
             arr[i] =  Math.min(arr[i],arr[i-1]+1);
-           
         }
+       
        
         return arr[arr.length-1];
     }
