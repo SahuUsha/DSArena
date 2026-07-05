23838c57f92ba8ebb5257dbccf8a131ef8ac9da3
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-07-05 22:58:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -1,124 +1,47 @@
 class Solution {
     // Function to calculate the number of subsets with a given sum
+    int ans = 0;
     public int perfectSum(int[] nums, int target) {
         // code here
+    //   return PerfectSum1(nums,target,0);
+    
+    return tabulation(nums,target);
+        // return ans;
+    }
+    
+    public int PerfectSum1(int []nums , int target, int idx){
+         if (idx == nums.length) {
+        return target == 0 ? 1 : 0;
+      }
         
+        int notTake =  PerfectSum1(nums, target, idx+1);
         
-        // int dp[][] = new int[nums.length][target+1];
-        
-        
-        // for(int[] pd : dp){
+        int Take =0;
+        if(nums[idx]<=target){
             
-        // Arrays.fill(pd,-1);
-        // }
+       Take =  PerfectSum1(nums,target-nums[idx],idx+1);
+        }
         
-        // return ksubset2(nums,target,0,dp);
-        
-        return tabulation(nums,target);
-        
-        
+        return notTake + Take; 
     }
     
-    // public int ksubset(int[] nums , int target ,int sum, int idx){
+    public int tabulation(int []nums, int target){
+        int [][]dp = new int[nums.length+1][target+1];
         
+        dp[0][0] =1;
         
-    //     if (idx == nums.length) {
-
-    //     if (sum == target) {
-    //         return 1;
-    //     }
-    //     return 0;
-    // }
-
-        
-    //     int notake = ksubset(nums,target,sum,idx+1);
-        
-    //     int take = ksubset(nums,target,sum + nums[idx], idx+1);
-        
-    //     return notake + take;
-    // }
-     public int ksubset(int[] nums , int target ,int sum, int idx){
-        
-        
-        if (idx == nums.length) {
-
-        if (sum == target) {
-            return 1;
-        }
-        return 0;
-    }
-
-        
-        int notake = ksubset(nums,target,sum,idx+1);
-        
-        int take = ksubset(nums,target,sum + nums[idx], idx+1);
-        
-        return notake + take;
-    }
-    
-    
-    public int ksubset2(int[] nums , int target ,int idx, int [][]dp){
-        
-        if (idx == nums.length) {
-        
-        if (target==0) {
-            return 1;
-        }
-        return 0;
-        }
-
-        if(dp[idx][target]!=-1){
-            return dp[idx][target];
-        }
-        
-        int notake = ksubset2(nums,target,idx+1,dp);
-        
-        int take = 0;
-        if(target>=nums[idx]){
-            
-        take = ksubset2(nums,target-nums[idx], idx+1,dp);
-        }
-        
-        
-        dp[idx][target] = notake + take;
-        
-        return dp[idx][target];
-    }
-    
-    public int tabulation(int []nums,int target){
-        
-        int [][]dp = new int[nums.length][target+1];
-        
-        
-        if(nums[0]==0){
-            dp[0][0] = 2;
-        }else{
-            dp[0][0] =1;
-        }
-        
-        
-        if(nums[0] != 0 && nums[0]<=target){
-            dp[0][nums[0]] = 1;
-        }
-        
-        // for(int i = 0 ; i<nums.length;i++){
-        //     dp[i][0] = 1;
-        // }
-        
-        for(int i = 1 ;i<nums.length ;i++){
-            for(int tar = 0 ; tar<=target;tar++){
-                int notTake = dp[i-1][tar];
+        for(int i = 1 ; i<=nums.length ; i++){
+            for(int tar = 0; tar<=target ;tar++){
                 
-                int take =0;
+                dp[i][tar] = dp[i-1][tar];
                 
-                if(nums[i]<=tar){
-                    take = dp[i-1][tar-nums[i]];
+                if(nums[i-1]<=tar){
+                    dp[i][tar] += dp[i-1][tar-nums[i-1]];
+                    
                 }
-                
-                dp[i][tar] = notTake+take;
             }
         }
-        return dp[nums.length-1][target];
+        
+        return dp[nums.length][target];
     }
-    
 }
