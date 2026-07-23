6496905e2d321bab2eb3ad1653696b45b58a40cd
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-07-23 21:56:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.java
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.java
@@ -1,67 +1,67 @@
 class Solution {
+    
     class Pair{
         int row;
         int col;
-        int dis;
-        Pair(int row, int col, int dis){
+        
+        Pair(int row, int col){
             this.row = row;
             this.col = col;
-            this.dis = dis;
         }
     }
     
     public ArrayList<ArrayList<Integer>> nearest(int[][] grid) {
         // code here
-        
         Queue<Pair> queue = new LinkedList<>();
-        int [][]visited = new int[grid.length][grid[0].length];
-        
         int [][]result = new int[grid.length][grid[0].length];
+        boolean visited[][] = new boolean[grid.length][grid[0].length];
         
-        for(int i = 0 ;i<grid.length;i++){
-            for(int j  =0 ;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    queue.add(new Pair(i,j,0));
-                    visited[i][j] = 1;
+        for(int i =0 ; i<grid.length;i++){
+            for(int j =0 ; j<grid[0].length; j++){
+                if(!visited[i][j] && grid[i][j]==1){
+                    queue.add(new Pair(i,j));
+                    visited[i][j] = true;
+                    
                 }
             }
         }
         
-        int []drow={-1,0,1,0};
-        int []dcol ={0,1,0,-1};
+        
+        int drow[] = {-1, 0, 1,0};
+        int dcol[] = {0,-1,0,1};
+        
         
         while(!queue.isEmpty()){
+            Pair p = queue.poll();
+            int row = p.row;
+            int col = p.col;
             
-            Pair pr = queue.poll();
-            
-            int row = pr.row;
-            int col = pr.col;
-            
-            for(int i = 0 ; i<4;i++){
-                int newRow = row+drow[i];
-                int newCol = col+dcol[i];
+            for(int i = 0 ; i<4 ; i++){
+                int newRow = row + drow[i];
+                int newCol = col + dcol[i];
                 
+                if(newRow>=0 && newCol>=0 && newRow<grid.length && newCol<grid[0].length)
+                {
+                    
+                if(!visited[newRow][newCol]){
+                    result[newRow][newCol] = result[row][col] + 1;
+                    queue.add(new Pair(newRow, newCol));
+                    visited[newRow][newCol] = true;
+                }
                 
-                if(newRow>=0 && newRow<grid.length && 
-                newCol>=0 && newCol<grid[0].length && visited[newRow][newCol]==0 ){
-                    visited[newRow][newCol] = 1;
-                    queue.add(new Pair(newRow,newCol,pr.dis+1));
-                    result[newRow][newCol] = pr.dis+1;
                 }
             }
         }
         
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        
-        for(int i  = 0 ; i<grid.length;i++){
-            list.add(new ArrayList<>());
-            for(int j = 0 ;j<grid[0].length;j++){
-                list.get(i).add(result[i][j]);
+        ArrayList<ArrayList<Integer>>  list = new ArrayList<>() ;
+        for(int i  = 0 ; i<result.length ; i++){
+            ArrayList<Integer> res= new ArrayList<>();
+            for(int j = 0 ; j<result[0].length ; j++){
+                res.add(result[i][j]);
             }
+            list.add(res);
         }
         
-        return list;
+        return  list;
     }
-    
-    
 }
