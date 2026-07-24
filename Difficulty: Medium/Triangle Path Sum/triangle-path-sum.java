9d5e8277205ad0f8cf198f3e0b87717517d6e415
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-07-24 15:19:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triangle Path Sum/triangle-path-sum.java
+++ Difficulty: Medium/Triangle Path Sum/triangle-path-sum.java
@@ -1,71 +1,43 @@
 class Solution {
-    public int minPathSum(ArrayList<ArrayList<Integer>> triangle ) {
+    public int minPathSum(ArrayList<ArrayList<Integer>> triangle) {
         // Code here
-        // return minrec(triangle,0,0);
-        
-        // int dp[][] = new int[triangle.size()][triangle.size()];
-        // for(int []d: dp){
-        //     Arrays.fill(d,-1);
-        // }
-        
-        // return memoization(triangle,0,0,dp);
+        // return pathSum(triangle, 0 , 0 ,0);
+        // int n = triangle.size();
         
         return tabulation(triangle);
+   
         
-    } 
-    public int minrec(ArrayList<ArrayList<Integer>> triangle  , int idx,int j){
-       
-          if (idx == triangle.size() - 1) {
-        return triangle.get(idx).get(j);
+        
     }
-
+    public int pathSum(ArrayList<ArrayList<Integer>> triangle,int level,int idx1 , int sum){
+        if(level>=triangle.size() ){
+            return sum;
+        }
         
-      
-          int cdown = triangle.get(idx).get(j) + minrec(triangle, idx + 1, j);
-          int diag = triangle.get(idx).get(j) + minrec(triangle,idx+1,j+1);
-          
-             return Math.min(cdown, diag);
         
-            
+      int notTake = pathSum(triangle,level+1,idx1,sum + triangle.get(level).get(idx1));
+      int take = pathSum(triangle, level+1, idx1+1, sum+triangle.get(level).get(idx1));
+        
+        return Math.min(notTake, take);
+        
     }
     
-    public int memoization(ArrayList<ArrayList<Integer>> triangle  , int idx,int j , int [][]dp){
-       
-           if (idx == triangle.size() - 1) {
-        return triangle.get(idx).get(j);
-        }
-        
-        if(dp[idx][j]!=-1){
-            return dp[idx][j];
-        }
-        
-         int cdown = triangle.get(idx).get(j) + minrec(triangle, idx + 1, j);
-         int diag = triangle.get(idx).get(j) + minrec(triangle,idx+1,j+1);
-          
-         dp[idx][j] = Math.min(cdown, diag);
-         return dp[idx][j];
-            
-    }
     public int tabulation(ArrayList<ArrayList<Integer>> triangle){
-        int n = triangle.size();
         
-        int [][]dp = new int[n][n];
-        
-        for(int i = 0 ;i<triangle.get(n-1).size();i++){
-            dp[n-1][i] = triangle.get(n-1).get(i);
-        }
-        
-        for(int i = n-2 ; i>=0 ;i--){
+           int n = triangle.size();
             
-            for(int j= i ; j>=0;j--){
-    
-                int cdown = triangle.get(i).get(j) + dp[i+1][j];
-                int up = triangle.get(i).get(j) + dp[i+1][j+1];
-            int min = Math.min(cdown,up);
-            dp[i][j] = min;
+            int dp[] = new int[n];
+            
+            for(int  i = 0 ; i<triangle.get(n-1).size() ; i++){
+                dp[i] =  triangle.get(n-1).get(i);
             }
-        }
-        return dp[0][0];
-        
+            
+            for(int row = n-2 ;row >=0 ; row--){
+                for(int col = 0 ; col<=row ; col++){
+                    dp[col] = triangle.get(row).get(col) + Math.min(dp[col], dp[col+1]);
+                }
+            }
+            
+            return dp[0];
     }
 }
