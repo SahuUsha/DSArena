b3cb5ee87802ee7fea5e6a1903c65020ba6fc10d
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-07-26 18:18:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -1,38 +1,29 @@
 class Solution {
     static int lis(int arr[]) {
         
-        int dp[][] = new int[arr.length][arr.length+1];
-        for(int[]d : dp){
-        Arrays.fill(d,-1);
-        }
-        // code here
-        return longestRecur(0,-1,arr,dp);
-    }
-    
-    int max = Integer.MIN_VALUE;
-    
-    public static int longestRecur(int idx, int prevIdx , int []arr, int dp[][]){
-         if(idx==arr.length ){
-            //  max = Math.max(len);
-             return 0;
-         }
-         
-         if(dp[idx][prevIdx+1]!=-1){
-             return dp[idx][prevIdx+1];
-         }
-         
-         int take = 0;
-         
-         if(prevIdx==-1 || arr[idx]>arr[prevIdx]){
-            take = 1+longestRecur(idx+1,idx,arr,dp);
-         }
-         
-         int notTake = longestRecur(idx+1,prevIdx,arr,dp);
-         
-         dp[idx][prevIdx+1] = Math.max(take,notTake);
         
-         return dp[idx][prevIdx+1];
+        int max = 0;
+        
+        
+        int cnt[] = new int[arr.length];
+        Arrays.fill(cnt,1);
+        
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int prev = 0 ; prev<i ; prev++){
+                
+                if(arr[i]>arr[prev] && cnt[prev]+1 > cnt[i]){
+                    cnt[i] = cnt[prev] + 1;
+                }
+                
+            }
+            
+                if(cnt[i]>max){
+                    max = cnt[i];
+                }
+            
+        }
+        
+        return max;
+        
     }
-    
-    
 }
