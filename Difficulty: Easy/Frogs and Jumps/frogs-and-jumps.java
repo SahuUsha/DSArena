f1178ba307ec793040609360c94bb509e0d81d7a
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-04 22:00:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frogs and Jumps/frogs-and-jumps.java
+++ Difficulty: Easy/Frogs and Jumps/frogs-and-jumps.java
@@ -1,27 +1,35 @@
 class Solution {
-    public int unvisitedLeaves(int N, int leaves, int frogs[]) {
-        // Code here
-        boolean []visited = new boolean[leaves+1];
-       
+    
+    int unvisitedLeaves(int arr[], int k) {
+        // code here
         
+        boolean []visited = new boolean[k+1];
         
-       for (int frog : frogs) {
-    if (frog > leaves || visited[frog]) {
-        continue;
-    }
-
-    for (int j = frog; j <= leaves; j += frog) {
-        visited[j] = true;
-    }
-}
+        for(int num : arr){
+            
+            
+            if(num>k){
+                continue;
+            }
+            
+            
+            for(int i = num ; i<=k ; i +=num){
+                visited[i] = true;
+            }
+            
+        }
         
         int count = 0;
-        for(int i =1;i<visited.length;i++){
-            if(visited[i]==false){
+        
+        for(int i = 1 ; i<visited.length ; i++){
+            if(!visited[i]){
                 count++;
             }
         }
         
+        
         return count;
     }
+    
+    
 }
