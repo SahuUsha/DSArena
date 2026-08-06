acd74f4d8e5e9a6f8d9e40273c2ba8ffa3eb61e4
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-06 20:24:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxChocolate(int grid[][]) {
         // code here
         
-    int dp[][][] = new int[grid.length][grid[0].length][grid[0].length];
+          int dp[][][] = new int[grid.length][grid[0].length][grid[0].length];
     for(int i = 0 ; i<dp.length ; i++){
         for(int j = 0 ; j<dp[0].length;j++){
             for(int k = 0 ; k<dp[0].length ; k++){
@@ -10,46 +10,46 @@ class Solution {
             }
         }
     }
-    
-    return Chocolate(grid,0,0,grid[0].length-1,dp);
         
+        return  maxChocolate(grid,0,0,grid[0].length-1,dp);
+      
     }
     
-  public int Chocolate(int[][] grid, int row, int lcol , int rcol, int[][][]dp) {
-      
-      if(lcol<0 || rcol<0 || lcol>=grid[0].length || rcol>=grid[0].length){
-          return Integer.MIN_VALUE;
-      }
-      
-      if(row==grid.length-1){
-          if(lcol==rcol) return grid[row][rcol];
-          else
-          return grid[row][lcol] + grid[row][rcol];
-      }
-      
-      if(dp[row][lcol][rcol]!=-1){
-          return dp[row][lcol][rcol];
-      }
-      
-      int []dj = {-1, 0, +1};
-      int maxi = Integer.MIN_VALUE;
-      for(int dj1 = -1 ; dj1<=1 ; dj1++){
-          int value= 0;
-          for(int dj2 = -1 ; dj2<=1 ; dj2++){
-              if(lcol==rcol){
-                  value = grid[row][rcol];
-              }else{
-                  value = grid[row][lcol] + grid[row][rcol];
-              }
-              value += Chocolate(grid,row+1,lcol+dj1,rcol+dj2,dp);
-              maxi = Math.max(maxi,value);
-              
-          }
-      }
-        dp[row][lcol][rcol] = maxi;
-      
-     return dp[row][lcol][rcol];
-  }
-
+    public int maxChocolate(int grid[][] , int row , int left, int right, int [][][]dp){
+        if(left< 0 || right<0 || right>=grid[row].length || left >=grid[row].length){
+            return Integer.MIN_VALUE;
+        }
+        if(row==grid.length-1){
+            if(left==right){
+                return grid[row][left];
+            }else{
+                return grid[row][left] + grid[row][right];
+            }
+        }
+        
+        if(dp[row][left][right]!=-1){
+            return dp[row][left][right];
+        }
+        
+         int max = Integer.MIN_VALUE;
+        for(int i  = -1 ; i<=1 ; i++){
+            int value = 0;
+            for(int j = -1; j<=1 ; j++){
+                if(left==right){
+                    value = grid[row][left];
+                }else{
+                    value = grid[row][left] + grid[row][right];
+                }
+                value += maxChocolate(grid,row+1,left+i,right+j,dp);
+                
+        max= Math.max(max ,value);
+            }
+            
+            dp[row][left][right] = max;
+        }
+        
+        return   dp[row][left][right] ;
+    }
+    
     
 }
