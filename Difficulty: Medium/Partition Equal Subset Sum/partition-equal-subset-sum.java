e289b55040b35ca6e0ff52bf4fbbb70d57ca8c49
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-06 21:41:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.java
@@ -1,30 +1,44 @@
 class Solution {
     static boolean equalPartition(int arr[]) {
+        
         // code here
+     int totalSum = 0;
+     for(int i = 0 ; i<arr.length ; i++){
+         totalSum  += arr[i];
+     }
+     if(totalSum%2!=0){
+         return false;
+     }
+     
+     int tar = totalSum/2;
+     
+     
+     return equalPart(arr,tar);
         
+    }
+    
+    public static boolean equalPart(int arr[], int tar){
         
-      int totalSum = 0;
-      for(int i = 0 ; i<arr.length ;i++){
-          totalSum += arr[i];
-      }
-      
-      if(totalSum%2!=0){
-          return false;
-      }
-      
-      int target = totalSum/2;
-      
-      
-      
-      boolean dp[] = new boolean[target+1];
-      dp[0] = true;
-      
-      for(int num : arr){
-          for(int i = target; i>=num ;i--){
-              dp[i] = dp[i] || dp[i-num];
-          }
-      }
-      
-      return dp[target];
+    boolean dp[][] =  new boolean[arr.length][tar+1];
+        
+    for(int i = 0 ; i<arr.length ;i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=tar){
+        dp[0][arr[0]] = true;
+    }
+    
+    for(int i = 1  ; i<arr.length ; i++){
+        for(int j = 0 ; j<=tar ; j++){
+            if(j-arr[i]>=0){
+                dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i]];
+            }else{
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+    
+   return dp[arr.length-1][tar];
     }
 }
