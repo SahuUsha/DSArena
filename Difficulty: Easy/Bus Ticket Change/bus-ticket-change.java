9addd0af8e58cef7ab30cb1fcbc53d2f5ee2744a
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-06 23:11:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bus Ticket Change/bus-ticket-change.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canServe(int[] arr) {
+        // code here
+        
+        int five = 0;
+        int ten = 0 ;
+        
+        for(int i = 0 ; i<arr.length; i++){
+            
+            if(arr[i]==5){
+                five++;
+            }else if(arr[i]==10){
+                if(five>0){
+                    five--;
+                    ten++;
+                }else{
+                    return false; 
+                }
+            }
+            else if(arr[i]==20){
+                if(five>0 && ten>0){
+                    five--;
+                    ten--;
+                }else if(five>=3){
+                    five -=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
