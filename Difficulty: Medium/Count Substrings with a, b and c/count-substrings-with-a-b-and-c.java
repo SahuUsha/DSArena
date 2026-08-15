cb74c59568ccadede0db893cb2f6e8c621d23669
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-15 15:39:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Substrings with a, b and c/count-substrings-with-a-b-and-c.java
+++ Difficulty: Medium/Count Substrings with a, b and c/count-substrings-with-a-b-and-c.java
@@ -2,25 +2,24 @@ class Solution {
     public static int countSubstring(String s) {
         // code here
         
-        int cnt =0;
         
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int last[] = {-1,-1,-1};
+        int cnt = 0;
+        int min = Integer.MAX_VALUE;
         
         
-        for(int i =0 ;i<s.length();i++){
-            map.put(0,0);
-            map.put(1,0);
-            map.put(2,0);
-            for(int j = i ;j<s.length();j++){
-                
-                map.put(s.charAt(j)-'a',1);
-                
-                if(map.get(0)+map.get(1)+map.get(2)==3){
-                    cnt = cnt+(s.length()-j);
-                    break;
-                }
+        for(int i = 0 ; i<s.length() ; i++){
+            
+            last[s.charAt(i)-'a'] =i;
+            
+            min = Math.min(last[0] , Math.min(last[1], last[2]));
+            
+            if(min!=-1){
+                cnt +=(1+min);
             }
         }
+        
+        
         return cnt;
         
     }
