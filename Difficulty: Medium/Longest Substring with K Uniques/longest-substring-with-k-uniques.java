8b8ae6f2c8fecd7153b786fa54917de4ddf1ddb6
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-16 23:24:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.java
@@ -1,77 +1,37 @@
 class Solution {
     public int longestKSubstr(String s, int k) {
-        return kDistinctChars(k,s);
-        
-    }
-    public int longestKSubstr1(String s, int k) {
         // code here
+        return longestKSubstr1(s,k);
         
-        
-    
-     Set<Character> set = new HashSet<>();
-     HashMap<Character, Integer> map = new HashMap<>();
-     
-     int r  = 0 ;
-     int l = 0;
-     
-     int MaxLen = 0;
-     
-     while(r<s.length()){
-         char ch =  s.charAt(r);
-         set.add(ch);
-         map.put(ch,map.getOrDefault(ch,0)+1);
-         
-         while(set.size()>k){
-               ch = s.charAt(l);
-             map.put(ch,map.getOrDefault(ch,0)-1);
-             if(map.get(ch)==0){
-                 set.remove(ch);
-             }
-         MaxLen = Math.max(MaxLen,r-l+1);
-             l++;
-         }
-         
-         r++;
-         
-     }
-     if(s.length()==k && MaxLen==0){
-          return s.length();
-     }
-     
-     return MaxLen==0? -1 : MaxLen;
-     
-     
-     
     }
-    public static int kDistinctChars(int k, String str) {
-		// Write your code here
-	int p = k;
-		// Write your code here
-		HashMap<Character, Integer> map = new HashMap<>();
-		Set<Character> set  =new HashSet<>();
-		int maxLen = 0;
-		int l = 0 , r=0;
-		while(r<str.length()){
-           
-		   map.put(str.charAt(r),map.getOrDefault(str.charAt(r),0)+1);
-		   set.add(str.charAt(r));
+    public static int longestKSubstr1(String s, int k) {
+        // code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        int len = -1;
+        
+        int  l = 0 , r =0;
+        
+        while(l<s.length() && r<s.length()){
+            
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
+            
+           while (map.size()>k){
+                
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                
+                if(map.get(s.charAt(l))==0){
+                    map.remove(s.charAt(l));
+                }
 
-		   while(map.size()>k){
-			   map.put(str.charAt(l),map.get(str.charAt(l))-1);
-
-			   if(map.get(str.charAt(l))==0){
-				   map.remove(str.charAt(l));
-				   set.remove(str.charAt(l));
-			   }
-			   l++;
-		   }
-
-            if(set.size()==p){
-		   maxLen = Math.max(maxLen,r-l+1);
-			}
-		   r++;
-
-		}
-		return maxLen==0?-1:maxLen;
-	}
+                l++;
+            }
+            
+           if (map.size() == k) {
+               len = Math.max(len, r - l + 1);
+           } 
+            r++;
+        }
+        
+        return len;
+    }
 }
