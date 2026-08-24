8380bfb3c1fdb69bf7f88fb68a9ec25494034294
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-24 22:23:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Insertions for Palindrome/minimum-insertions-for-palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    static int findMinInsertions(String s) {
+        // code here
+           
+ StringBuilder str = new StringBuilder(s);
+String revStr = str.reverse().toString();
+
+     int dp[][] = new int[s.length()+1][s.length()+1];
+
+         for(int i = 1 ; i<=s.length() ; i++){
+     for(int j = 1 ; j <=s.length() ; j++){
+
+          if(s.charAt(i-1)==revStr.charAt(j-1)){
+             dp[i][j] = dp[i-1][j-1]+1;
+         }else{
+         dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+
+       }
+
+     }
+ }
+
+  return s.length() -  dp[s.length()][s.length()];
+            
+            
+            
+            
+    }
+}
