e0f1915b2c2847dff5965b6677f75ed913d4c77e
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-08-31 17:08:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/XOR of a Number Range/xor-of-a-number-range.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int findXOR(int l, int r) {
+        // code here
+       return xorTillN(r) ^ xorTillN(l-1);
+    }
+    
+    public static int xorTillN(int n){
+        
+        if(n%4==0) return n;
+        if(n%4==1) return 1;
+        if(n%4==2) return n+1;
+        
+        return 0;
+    }
+}
