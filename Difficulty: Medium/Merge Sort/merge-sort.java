1270429b78a81adf5b8b6ecc5b17935058ae2c4a
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-09-11 23:49:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.java
+++ Difficulty: Medium/Merge Sort/merge-sort.java
@@ -1,60 +1,64 @@
 class Solution {
-
-  void mergeSort(int arr[], int l, int r){
-     mergeSort1(arr,l,r);
-      
-      
-  }
-  
-
-
-   private static void mergeSort1(int arr[], int l, int r) {
+    public void mergeSort(int arr[], int l, int r) {
         // code here
-         if (l>= r)
-            return;
+        if(l<r){
+            
+        int mid = (l +r )/2;
         
-        int mid = (l+r)/2;
+        mergeSort(arr, l , mid);
+        mergeSort(arr, mid+1 ,r);
         
-        mergeSort1(arr,l,mid);
-        mergeSort1(arr,mid+1,r);
+        merge(arr, l , mid, r);
+        }
         
-        merge(arr,l,mid,r);
         
     }
     
-    private static void merge(int arr[], int left , int mid, int right){
+    public void merge (int[] arr , int l , int m , int r){
         
-        int i = left;
-        int j = mid+1;
-        int k = 0;
+        int n1 = m- l +1;
+        int n2 = r-m;
         
-        int len = right-left+1;
+        int L[] = new int[n1];
+        int R[] = new int[n2];
         
-        int temp[] = new int[len];
+        for(int i =0 ;i<n1;i++){
+            L[i] = arr[l+i] ;
+        }
         
-        while(i<=mid && j<=right){
-            if(arr[i]<=arr[j]){
-                temp[k++] = arr[i++];
+        for(int i = 0 ; i<n2 ; i++){
+            R[i] = arr[m + 1+i];
+        }
+        
+        
+        int i = 0 ;
+        int j = 0;
+        int k = l;
+        
+        while(i<n1 && j<n2){
+            
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
             }else{
-                temp[k++] = arr[j++];
+                arr[k] = R[j];
+                j++;
             }
+            k++;
         }
         
-        
-        while(i<=mid){
-            temp[k++] = arr[i++];
+        while(j<n2){
+            arr[k] = R[j];
+            j++;
+            k++;
         }
         
-        while(j<=right){
-            temp[k++] = arr[j++];
+        while(i<n1){
+            arr[k] = L[i];
+            i++;
+            k++;
         }
-        
-        for(int p = 0 ; p<len ;p++){
-            arr[left+p] = temp[p];
-        }
-        
         
     }
-    
     
 }
