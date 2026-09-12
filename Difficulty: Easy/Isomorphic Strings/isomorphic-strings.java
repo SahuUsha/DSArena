1918653555aa724bc32a9c4c2e97b1cd1885f136
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-09-12 16:42:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -1,26 +1,37 @@
 class Solution {
-    public boolean areIsomorphic(String s, String t) {
+    public boolean areIsomorphic(String s1, String s2) {
         // code here
-       int map1[] = new int[200];
-       int map2[] = new int[200];
-
-       if(s.length()!=t.length()){
-        return false;
-       }
         
-    for(int i =0 ;i <s.length();i++){
-
-        if(map1[s.charAt(i)] != map2[t.charAt(i)]){
+        if(s1.length()!=s2.length()){
             return false;
         }
-
-        map1[s.charAt(i)] = i+1;
-        map2[t.charAt(i)] = i+1;
-
-
-
-    }
-    return true;
+   
+   HashMap<Character, Character> map1 = new HashMap<>();
+   HashMap<Character , Character> map2 = new HashMap<>();
+   
+    
+    for(int i = 0 ; i<s1.length() ; i++){
         
+       char ch1 = s1.charAt(i);
+       char ch2 = s2.charAt(i);
+       
+       if(map1.containsKey(ch1)){
+           
+           if(map1.get(ch1)!=ch2){
+               return false;
+           }
+       }
+       else{
+           if(map2.containsKey(ch2)){
+               return false;
+           }
+           
+           map1.put(ch1,ch2);
+           map2.put(ch2,ch1);
+       }
+    }
+        
+        
+        return true;
     }
 }
