3fdc2d1cff96a379a4b349615ce78f3b8820f1db
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2026-09-21 23:08:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
+++ Difficulty: Easy/Stock Buy and Sell – Max one Transaction Allowed/stock-buy-and-sell-max-one-transaction-allowed.java
@@ -2,15 +2,48 @@ class Solution {
     public int maxProfit(int[] prices) {
         // Code here
         
-        int buy = prices[0];
-        int max = 0;
+        return maxProfit1(prices);
         
-        for(int i = 1; i<prices.length ;i++){
-          int cost = prices[i] - buy;
-            max  = Math.max(max,cost);
-            buy = Math.min(buy,prices[i]);
-        }
-        
-        return max;
     }
+    
+    public int maxProfit1(int []prices){
+        
+        int minPrices = prices[0];
+        int profit = 0;
+        
+        for(int  i =1 ; i<prices.length ; i++){
+            minPrices = Math.min(minPrices,prices[i]);
+            profit = Math.max(profit, prices[i]-minPrices);
+            
+        }
+        return profit;
+        
+    }
+    
+//   public int maxProfitTabulate(int[] prices) {
+
+//       int n = prices.length;
+//       int dp[][] = new int[n][n];
+
+//       // Buy on day 0
+//       for(int j = 1; j < n; j++) {
+//           dp[0][j] = Math.max(dp[0][j-1], prices[j] - prices[0]);
+//       }
+
+//       for(int i = 1; i < n; i++) {
+
+//           for(int j = i + 1; j < n; j++) {
+
+//               dp[i][j] = Math.max(
+//                   dp[i-1][j],
+//                   Math.max(
+//                       dp[i][j-1],
+//                       prices[j] - prices[i]
+//                   )
+//               );
+//           }
+//       }
+
+//       return dp[n-1][n-1];
+//   }
 }
